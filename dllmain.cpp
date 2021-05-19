9031ee74379b022d75e30c62bbbc744dd71926a4
Qi You <1425321705@qq.com>
2021-05-20 00:10:52 +0800

    Fix CRT exception escape

--- dllmain.cpp
+++ dllmain.cpp
@@ -3,14 +3,15 @@
 #include <cstdio>
 #include <cstdlib>
 #include <crtdbg.h>
+#include <eh.h>
 #include <cstddef>
 #include <cstdarg>
 #include <cstring>
-#include <ctime>
 
 #define MAX_STACK_FRAMES 50
 #define LOG_OUTPUT_PATH ".\\logs\\TrackBack.log"
 #define DUMP_OUTPUT_PATH L".\\logs\\CrashDump.dmp"
+#define CRT_ERR_CODE 0xE0000001
 
 FILE* fLog;
 bool inSEH = true;
@@ -29,24 +30,27 @@ void log(const char* format, ...)
     va_end(args);
 }
 
+// CRT exception
 void CrtInvalidParameterHandler(const wchar_t* expression,const wchar_t* function,
     const wchar_t* file, unsigned int line, uintptr_t pReserved)
 {
-    //log("[CrashLogger] CRT catch exception.\n");
-    throw 1;
+    RaiseException(CRT_ERR_CODE, EXCEPTION_NONCONTINUABLE, 0, NULL);
 }
-void CrtPurecallHandler(void)
+void CrtPurecallHandler()
 {
-    //log("[CrashLogger] CRT catch exception.\n");
-    throw 1;
+    RaiseException(CRT_ERR_CODE, EXCEPTION_NONCONTINUABLE, 0, NULL);
+}
+void CrtTerminationHandler()
+{
+    RaiseException(CRT_ERR_CODE, EXCEPTION_NONCONTINUABLE, 0, NULL);
 }
 
 LONG WINAPI CrashLogger(PEXCEPTION_POINTERS pe)
 {
-    //log("[CrashLogger] SEH catch exception.\n");
     HANDLE hProcess = GetCurrentProcess();
     HANDLE hThread = GetCurrentThread();
     CreateDirectory(L"logs", NULL);
+
     errno_t res = fopen_s(&fLog, LOG_OUTPUT_PATH, "a");
     if (res != 0)
     {
@@ -91,7 +95,6 @@ LONG WINAPI CrashLogger(PEXCEPTION_POINTERS pe)
             log("[TrackBack] At File %s : Line %d \n", line.FileName, line.LineNumber);
     }
     SymCleanup(hProcess);
-    log("\n");
 
     ////////// CrashDump //////////
     HANDLE hDumpFile = CreateFile(DUMP_OUTPUT_PATH, GENERIC_WRITE, 0, NULL, 
@@ -104,9 +107,23 @@ LONG WINAPI CrashLogger(PEXCEPTION_POINTERS pe)
         dumpInfo.ClientPointers = TRUE;
         MiniDumpWriteDump(hProcess, GetCurrentProcessId(), hDumpFile, MiniDumpNormal,
             &dumpInfo, NULL, NULL);
+        log("[MiniDump] Minidump generated at %ls\n", DUMP_OUTPUT_PATH);
     }
+    log("\n");
+    if (fLog != NULL && fLog != INVALID_HANDLE_VALUE)
+        fclose(fLog);
 
     return EXCEPTION_CONTINUE_SEARCH;
+}
+
+void InitHandler()
+{
+    //SEH
+    SetUnhandledExceptionFilter(CrashLogger);
+    //CRT
+    _set_invalid_parameter_handler(CrtInvalidParameterHandler);
+    _set_purecall_handler(CrtPurecallHandler);
+    set_terminate(CrtTerminationHandler);
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -117,12 +134,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        //SEH
-        SetUnhandledExceptionFilter(CrashLogger);
-        //CRT
-        _set_invalid_parameter_handler(CrtInvalidParameterHandler);
-        _set_purecall_handler(CrtPurecallHandler);
-
+        InitHandler();
         printf("[CrashLogger] CrashLogger loaded.\n");
         break;
     case DLL_THREAD_ATTACH:
@@ -130,8 +142,6 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     case DLL_THREAD_DETACH:
         break;
     case DLL_PROCESS_DETACH:
-        if (fLog != NULL && fLog != INVALID_HANDLE_VALUE)
-            fclose(fLog);
         break;
     }
     return TRUE;
